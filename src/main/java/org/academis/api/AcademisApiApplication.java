ebf7a6306d8057f327a9238268069632c0f6628a
Rogerio J. Gentil <rogerio_gentil@yahoo.com.br>
2019-02-14 01:00:55 -0200

    Adiciona projeto base criado via Spring Initializr

---
+++ src/main/java/org/academis/api/AcademisApiApplication.java
@@ -0,0 +1,14 @@
+package org.academis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademisApiApplication.class, args);
+	}
+
+}
+
