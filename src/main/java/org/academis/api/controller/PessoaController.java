4c80ace92bf0966822bdf6b3ac4864cab0e11697
Rogerio J. Gentil <rogerio_gentil@yahoo.com.br>
2021-05-21 18:43:44 -0300

    Adiciona anotação ao controller de Pessoa

--- src/main/java/org/academis/api/controller/PessoaController.java
+++ src/main/java/org/academis/api/controller/PessoaController.java
@@ -5,9 +5,11 @@ import org.academis.api.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
+@RestController
 @RequestMapping(value = "pessoa")
 public class PessoaController {
 
@@ -18,4 +20,6 @@ public class PessoaController {
     public Collection<Pessoa> get() {
         return pessoaRepository.findAll();
     }
+
+
 }
